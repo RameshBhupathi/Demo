7b9f687f55237f33da90e053c9a1eb827a184865
RameshBhupathi <bhupatiramesh@gmail.com>
2019-04-22 00:47:44 +0530

    Showing the full article in the webview using article url in DetailsPage.

---
+++ app/src/main/java/io/ramesh/timesapidemo/view/articles_details/ArticleDetailsViewModel.kt
@@ -0,0 +1,14 @@
+package io.ramesh.timesapidemo.view.articles_details
+
+import androidx.lifecycle.ViewModel
+import io.ramesh.timesapidemo.api.repository.ProjectRepository
+import javax.inject.Inject
+
+
+/**
+ * Created by Ramesh Bhupathi on 2019-04-21.
+ */
+class ArticleDetailsViewModel @Inject constructor(private val repository: ProjectRepository) : ViewModel() {
+    var articleUrl: String? = null
+    var title: String? = null
+}
